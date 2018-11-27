c1c950d0cb31d2da4b945c8ff042e6be1c5fb236
Feathers <616510229@qq.com>
2018-11-27 11:11:15 +0800

    lang3

---
+++ lang3/src/test/java/lang3/EnumUtilsDemo.java
@@ -0,0 +1,20 @@
+package lang3;
+
+import org.junit.Test;
+
+/**
+ * EnumUtilsDemo
+ * 枚举工具类
+ * <p>
+ *
+ * @author Feathers
+ * @date 2018-05-22 9:09
+ */
+public class EnumUtilsDemo {
+
+    @Test
+    public void test() {
+
+    }
+
+}
