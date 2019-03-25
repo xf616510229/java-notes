69ea621172de4be1f84933d1becd28c5a0e4e78f
Feathers <616510229@qq.com>
2019-03-25 13:47:05 +0800

    jms transaction

--- mq/src/test/java/jms/transaction/TransactionConsumer.java
+++ mq/src/test/java/jms/transaction/TransactionConsumer.java
@@ -20,14 +20,13 @@ public class TransactionConsumer {
         try {
             c = cf.createConnection();
             c.start();
-            // 事务：事务为提交，不会应用更改，可以使用commit方法提交，rollback方法回滚
+            // 事务：事务未提交，不会确认消息，可以使用commit方法确认，rollback方法回滚
             
             // 发送端如果消息未确认，在队列/主题中不会出现该消息
             // 接收端如果消息未确认，在队列中不会移除该消息（主题无效）
             
-            // 参数1为true，代表创建一个事务性会话，此时参2（应答模式）才会生效
-            // 事务性会话需要调用commit方法对消息进行手动确认
-            // 事务性会话共有以下三种应答模式：
+            // 参数1为false，代表创建一个非事务性会话，此时参2（应答模式）才会生效
+            // 非事务性会话共有以下三种应答模式：
             // AUTO_ACKNOWLEDGE，自动确认，recive方法返回以后或者[MessageListener.onMessage] 方法成功返回以后，会话会自动确认消息
             // CLIENT_ACKNOWLEDGE，客户端确认，需要客户端通过消息对象textMessage.acknowledge()方法手动确认消息，确认时将会目前客户端消费的所有消息一起确认
             // DUPS_OK_ACKNOWLEDGE，延迟确认
